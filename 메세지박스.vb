87c010b8aa62c570fca7a92a2f4a0734b4739614
hadasjk <36807086+hadasjk@users.noreply.github.com>
2018-08-10 19:52:40 +0900

    Create 메세지박스.vb

---
+++ 메세지박스.vb
@@ -0,0 +1,10 @@
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = "hello word"
+
+    End Sub
+End Class
